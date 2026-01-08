dbf438cc69263bcd30bf364c5de324fa179cc5ec
Riya Panjwani <riyapanjwani8020@gmail.com>
2026-01-08 20:27:12 +0530

    Fix screens and exports

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: "/photobooth/",
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
